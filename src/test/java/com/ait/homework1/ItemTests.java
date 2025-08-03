7547708d31cc81257674f505bbcfc84b157b092d
Karina <karinailchenko455@gmail.com>
2025-08-03 20:27:36 +0200

    Last Change

--- src/test/java/com/ait/homework1/ItemTests.java
+++ src/test/java/com/ait/homework1/ItemTests.java
@@ -1,8 +1,5 @@
 package com.ait.homework1;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -10,26 +7,18 @@ public class ItemTests extends TestBases {
 
         @BeforeMethod
         public void precondition() {
-            click1(By.cssSelector("[href='/login']"));
-            type1(By.id("Email"), "rosaliaw1@de.com");
-            type1(By.id("Password"), "Qwert09_poi");
-            click1(By.id("RememberMe"));
-            click1(By.className("login-button"));
+            clickOnTheLogInButton();
+            clickOnTheEmailDueLogin();
+            clickOnThePasswordDueLogin();
+            clickOnTheRememberMeButton();
+            clickOnTheLoginButton();
         }
 
         @Test
-    public void addItemToCartTest() {
-            //click on the second product button Add to cart;
-            WebElement laptopLink = driver.findElement(By.xpath("//a[text()='14.1-inch Laptop']"));
-            laptopLink.click();
-
-            //click on the Shopping cart link in the header;
-            WebElement addToCartBtn = driver.findElement(By.id("add-to-cart-button-31"));
-            addToCartBtn.click();
-
-            //check the product added to the cart by its name;
-            Assert.assertTrue(isElementPresent(By.xpath("//a[text()='14.1-inch Laptop']")));
-
+         public void addItemToCartTest() {
+            clickOnTheSecondProduct();
+            clickOnTheAddToTheCart();
+            assertItemInCart();
         }
 
 }
--- src/test/java/com/ait/homework1/TestBases.java
+++ src/test/java/com/ait/homework1/TestBases.java
@@ -1,12 +1,10 @@
 package com.ait.homework1;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -60,6 +58,88 @@ public class TestBases {
 
     public void click1(By locator1) {
         driver.findElement(locator1).click();
+    }
+
+    public void assertCheckTheLogOutButton() {
+        Assert.assertTrue(isElementPresent(By.cssSelector("[href='/logout']")));
+    }
+
+    public void clickOnTheRegisterButton() {
+        click(By.id("register-button"));
+    }
+
+    public void clickOnThePasswordConfirmation() {
+        type(By.id("ConfirmPassword"), "Qwert09_poi");
+    }
+
+    public void clickOnTheLoginButton() {
+        click1(By.className("login-button"));
+    }
+
+    public void clickOnTheRememberMeButton() {
+        click1(By.id("RememberMe"));
+    }
+
+    public void clickOnThePassword() {
+        type(By.id("Password"), "Qwert09_poi");
+    }
+
+    public void clickOnTheEmail() {
+        type(By.id("Email"), newEmail());
+    }
+
+    public void clickOnTheLastName() {
+        type(By.id("LastName"), "Williams");
+    }
+
+    public void clickOnTheFirstName() {
+        type(By.id("FirstName"), "Rosalia");
+    }
+
+    public void clickOnTheGender() {
+        click(By.cssSelector("#gender-female"));
+    }
+
+    public void clickOnTheRegister() {
+        click(By.cssSelector("[href='/register']"));
+    }
+
+    public void clickOnTheLoginButtonDueLogin() {
+        driver.findElement(By.className("login-button")).click();
+    }
+
+    public void clickOnTheRememberMeDueLogin() {
+        driver.findElement(By.id("RememberMe")).click();
+    }
+
+    public void clickOnThePasswordDueLogin() {
+        type1(By.id("Password"), "Qwert09_poi");
+    }
+
+    public void clickOnTheEmailDueLogin() {
+        type1(By.id("Email"), "rosaliaw1@de.com");
+    }
+
+    public void clickOnTheLogin() {
+        click1(By.cssSelector("[href='/login']"));
+    }
+
+    public void clickOnTheLogInButton() {
+        clickOnTheLogin();
+    }
+
+    public void assertItemInCart() {
+        Assert.assertTrue(isElementPresent(By.xpath("//a[text()='14.1-inch Laptop']")));
+    }
+
+    public void clickOnTheAddToTheCart() {
+        WebElement addToCartBtn = driver.findElement(By.id("add-to-cart-button-31"));
+        addToCartBtn.click();
+    }
+
+    public void clickOnTheSecondProduct() {
+        WebElement laptopLink = driver.findElement(By.xpath("//a[text()='14.1-inch Laptop']"));
+        laptopLink.click();
     }
 }
 
