882847e0698b6b359b36240ddb05be25a13700ad
Karina <karinailchenko455@gmail.com>
2025-08-17 11:55:08 +0300

    Project Changes to homework 17

--- src/main/java/com/phonebook/core/BaseHelper.java
+++ src/main/java/com/phonebook/core/BaseHelper.java
@@ -1,25 +1,31 @@
 package com.phonebook.core;
 
+import com.google.common.io.Files;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
+
 public class BaseHelper {
 
-   protected WebDriver driver;
+    protected WebDriver driver;
 
     public BaseHelper(WebDriver driver) {
         this.driver = driver;
     }
 
     public String newEmail() {
-        int i =(int) (System.currentTimeMillis()/1000%3600);
+        int i = (int) (System.currentTimeMillis() / 1000 % 3600);
         String email = "manuel" + i + "@gmail.com";
         return email;
     }
 
-    public boolean isElementPresent(By locator){
-        return driver.findElements(locator).size()>0; // возвращает любой element
+    public boolean isElementPresent(By locator) {
+        return driver.findElements(locator).size() > 0; // возвращает любой element
     }
 
     public void type(By locator, String text) {
@@ -50,4 +56,14 @@ public class BaseHelper {
         }
     }
 
-}
+//    public String takeScreenshot() {
+//        File tmp = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+//        File screenshot = new File("screenshots/screen-" + System.currentTimeMillis() + ".png");
+//
+//        try {
+//            Files.copy(tmp,screenshot);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//        return screenshot.getAbsolutePath();
+    }
--- src/test/java/com/ait/homework1/core/TestBases.java
+++ src/test/java/com/ait/homework1/core/TestBases.java
@@ -2,22 +2,41 @@ package com.ait.homework1.core;
 
 import com.phonebook.core.ApplicationManager;
 import org.openqa.selenium.remote.Browser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBases {
 
-    protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", Browser.CHROME.browserName()));
+    protected ApplicationManager app;
 
-    @BeforeMethod
-    public void setUp(){
-        app.init();
+    Logger logger = LoggerFactory.getLogger(TestBases.class);
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp(Method method, Object[] p) {
+        logger.info("Start test {} with data: {}", method.getName(), Arrays.asList(p));
+        // новий ApplicationManager на кожен тест
+        app = new ApplicationManager(System.getProperty("browser", Browser.CHROME.browserName()));
+        app.init();   // тут створюється свіжий WebDriver і відкривається baseUrl
     }
 
-    @AfterMethod(enabled = false)
-    public void tearDown() {
-        app.stop();
-    }
+    @AfterMethod(alwaysRun = true)
+    public void tearDown(ITestResult result) {
+        if (result.isSuccess()) {
+            logger.info("PASSED: {}", result.getMethod().getMethodName());
+        } else {
+            logger.error("FAILED: {}", result.getMethod().getMethodName());
+        }
+        logger.info("Stop test");
+        logger.info("********************************************");
 
+        if (app != null) {
+            app.stop();  // закриває драйвер після кожного тесту
+        }
+    }
 }
-
